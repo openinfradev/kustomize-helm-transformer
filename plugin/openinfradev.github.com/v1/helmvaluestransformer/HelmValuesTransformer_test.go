c1a94b7bd8955946a68dcd7eaf0a5126894aca7e
JohnHaan <yongiman@gmail.com>
2020-07-08 06:36:47 +0000

    Add unittest

---
+++ plugin/openinfradev.github.com/v1/helmvaluestransformer/HelmValuesTransformer_test.go
@@ -0,0 +1,52 @@
+
+package main_test
+
+import (
+        "testing"
+
+        kusttest_test "sigs.k8s.io/kustomize/api/testutils/kusttest"
+)
+
+
+func TestHelmValuesTransformer(t *testing.T) {
+	th := kusttest_test.MakeEnhancedHarness(t).
+		BuildGoPlugin("openinfradev.github.com", "v1", "HelmValuesTransformer")
+	defer th.Reset()
+
+	rm := th.LoadAndRunTransformer(`
+apiVersion: openinfradev.github.com/v1
+kind: HelmValuesTransformer
+metadata:
+  name: site
+global:
+  docker_registry: registry.cicd.stg.taco
+helmValues:
+  - chartName: glance
+    chartRef: taco-k8s-v20.07
+`, `
+apiVersion: helm.fluxcd.io/v1
+kind: HelmRelease
+metadata:
+  name: glance
+spec:
+  chart:
+    git: https://tde.sktelecom.com/stash/scm/openstack/openstack-helm.git
+    path: glance
+    ref: master
+  releaseName: glance
+  targetNamespace: openstack
+`)
+	th.AssertActualEqualsExpected(rm, `
+apiVersion: helm.fluxcd.io/v1
+kind: HelmRelease
+metadata:
+  name: glance
+spec:
+  chart:
+    git: https://tde.sktelecom.com/stash/scm/openstack/openstack-helm.git
+    path: glance
+    ref: taco-k8s-v20.07
+  releaseName: glance
+  targetNamespace: openstack
+`)
+}
